4c05c414feb51db328ec13d1091c77e202aa7e25
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-05-24 19:32:50 -0300

    projeto-mario-servicos

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function cliqueNoBotao(){
-    alert("Cliquei no Botão!");
+const formulario = document.querySelector(".formulario")
+const mascara = document.querySelector(".mascara-formulario")
+
+function mostrarForm() {
+    formulario.style.left = "50%";
+    formulario.style.transform = "translateX(-50%)";
+    mascara.style.visibility = "visible"
 }
+
+function esconderForm() {
+    formulario.style.left = "-340%";
+    formulario.style.transform = "translateX(0%)";
+    mascara.style.visibility = "hidden";
+}
+
